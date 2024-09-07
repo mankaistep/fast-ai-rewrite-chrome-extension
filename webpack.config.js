53ddb55fa231779daccdeb093d502c3148178643
mankaistep <hhoang21vn@gmail.com>
2024-09-07 17:09:56 +0700

    add run build dev command

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,44 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-    mode: 'development', // Set this to 'production' for production builds
-    entry: {
-        popup: './src/popup.tsx',
-        background: './src/background.ts',
-        'content-script': './src/content-script.tsx'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.css']
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: 'public', to: '.' }]
-        }),
-    ],
-    devtool: 'cheap-module-source-map'
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        mode: isProduction ? 'production' : 'development',
+        entry: {
+            popup: './src/popup.tsx',
+            background: './src/background.ts',
+            'content-script': './src/content-script.tsx'
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js', '.css']
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [{ from: 'public', to: '.' }]
+            }),
+        ],
+        devtool: isProduction ? false : 'inline-source-map',
+        optimization: {
+            minimize: isProduction
+        }
+    };
 };
