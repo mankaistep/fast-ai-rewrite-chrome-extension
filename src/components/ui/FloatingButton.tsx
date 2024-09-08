7da7c6cb7076418ba824a5e703cf2211610304e2
mankaistep <hhoang21vn@gmail.com>
2024-09-08 16:43:24 +0700

    fix with Gmail

--- src/components/ui/FloatingButton.tsx
+++ src/components/ui/FloatingButton.tsx
@@ -8,16 +8,12 @@ interface FloatingButtonProps {
 }
 
 const FloatingButton: React.FC<FloatingButtonProps> = ({ position, onClick, addLog }) => {
-    const handleClick = useCallback((e: React.MouseEvent) => {
-        addLog('FloatingButton handleClick called');
-        e.preventDefault();
-        e.stopPropagation();
-        onClick();
-    }, [onClick, addLog]);
-
     const handleMouseDown = useCallback((e: React.MouseEvent) => {
         addLog('FloatingButton mousedown event');
-    }, [addLog]);
+        e.preventDefault();
+        e.nativeEvent.stopImmediatePropagation();
+        onClick();
+    }, [onClick, addLog]);
 
     const handleMouseUp = useCallback((e: React.MouseEvent) => {
         addLog('FloatingButton mouseup event');
@@ -25,7 +21,7 @@ const FloatingButton: React.FC<FloatingButtonProps> = ({ position, onClick, addL
 
     return (
         <div
-            className="fixed z-[9999]"
+            className="fixed z-[99999]"
             style={{
                 top: `${position.top}px`,
                 left: `${position.left}px`,
@@ -33,20 +29,16 @@ const FloatingButton: React.FC<FloatingButtonProps> = ({ position, onClick, addL
             onMouseEnter={() => addLog('Mouse entered FloatingButton')}
             onMouseDown={handleMouseDown}
             onMouseUp={handleMouseUp}
-            onClick={(e) => {
-                addLog('Div onClick event');
-                handleClick(e);
-            }}
         >
             <Button
-                onClick={handleClick}
                 onMouseDown={(e) => {
                     addLog('Button mousedown event');
-                    e.stopPropagation();
+                    e.nativeEvent.stopImmediatePropagation();
+                    handleMouseDown(e);
                 }}
                 onMouseUp={(e) => {
                     addLog('Button mouseup event');
-                    e.stopPropagation();
+                    e.nativeEvent.stopImmediatePropagation();
                 }}
                 variant="outline"
                 size="sm"
